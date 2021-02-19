f40d9d344c319f0bb4e86e24a51e24adaf6e409d
Simon Koll <simonkoll@admins-Mac-mini.local>
2021-02-19 16:49:19 +0100

    created Laughmeter with storyboard

--- Laughmeter/Laughmeter/AppDelegate.swift
+++ Laughmeter/Laughmeter/AppDelegate.swift
@@ -2,7 +2,8 @@
 //  AppDelegate.swift
 //  Laughmeter
 //
-
+//  Created by Simon Koll on 19.02.21.
+//
 
 import UIKit
 
---
+++ Laughmeter/Laughmeter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Laughmeter
+//
+//  Created by Simon Koll on 19.02.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
