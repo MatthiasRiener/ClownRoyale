2fe49ae1ef96aff02eeaaf233a5206312f28fb21
SimonKoll <simonkoll20@gmail.com>
2021-02-25 16:24:25 +0100

    Finished Laughmeter and close #30

---
+++ src/frontend/Loudnessdetection/Loudnessdetection/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  Loudnessdetection
+//
+//  Created by Simon Koll on 25.02.21.
+//
+
+import Foundation
--- src/frontend/Loudnessdetection/Loudnessdetection/ContentView.swift
+++ src/frontend/Loudnessdetection/Loudnessdetection/ContentView.swift
@@ -3,6 +3,33 @@ import SwiftUI
 
 var total: Float = 0.0
 
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+        case 6: // RGB (24-bit)
+            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        case 8: // ARGB (32-bit)
+            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+        default:
+            (a, r, g, b) = (1, 1, 1, 0)
+        }
+
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue:  Double(b) / 255,
+            opacity: Double(a) / 255
+        )
+    }
+}
+
 struct ProgressBar: View {
     var value: Float
 
@@ -11,10 +38,10 @@ struct ProgressBar: View {
             ZStack (alignment: .leading){
                 Rectangle().frame(width: geometry.size.width, height: geometry.size.height)
                     .opacity(0.3)
-                    .foregroundColor(Color(UIColor.systemTeal))
+                    .foregroundColor(Color(hex:"#ffB199"))
                 
                 Rectangle().frame(width: min(CGFloat(self.value) * geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.systemBlue))
+                    .foregroundColor(Color(hex: "#FFB400"))
             }.cornerRadius(45.0)
         }
     }
