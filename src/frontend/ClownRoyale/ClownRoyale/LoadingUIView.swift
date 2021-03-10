33be2451e4774433e1a1ac82d374c34a3d176d66
Jan Donnerbauer <donnerbauerjan@gmail.com>
2021-03-10 18:25:50 +0100

    Added Loading Page

---
+++ src/frontend/ClownRoyale/ClownRoyale/LoadingUIView.swift
@@ -0,0 +1,47 @@
+//
+//  LoadingUIView.swift
+//  ClownRoyale
+//
+//  Created by Jan Donnerbauer on 10.03.21.
+//
+
+import SwiftUI
+
+struct LoadingUIView: View {
+    var body: some View {
+
+        VStack(alignment: .center, spacing: 0) {
+            Image("bus")
+                .resizable()
+                .aspectRatio(contentMode:.fill)
+                .offset(x: 120, y: 0)
+                .ignoresSafeArea()
+            
+
+            Text("ClownRoyale")
+                .font(.title)
+                .fontWeight(.bold)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.white)
+                .padding()
+            .frame(
+                maxWidth: .infinity
+            )
+            .background(
+                LinearGradient(
+                    gradient: Gradient(
+                        colors: [Color("ClownYellow"),
+                                 Color("ClownYellowHell")]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                )
+            .edgesIgnoringSafeArea(.bottom))
+        }
+    }
+}
+
+struct LoadingUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingUIView()
+    }
+}
