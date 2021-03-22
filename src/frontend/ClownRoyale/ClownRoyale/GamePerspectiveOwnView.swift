df5ecc34871bd1d0a946d37bf2ca863be9e70243
Matthias Riener <matti.riener@gmail.com>
2021-03-22 16:01:51 +0100

    added game perspective screen

---
+++ src/frontend/ClownRoyale/ClownRoyale/GamePerspectiveOwnView.swift
@@ -0,0 +1,20 @@
+//
+//  GamePerspectiveOwnView.swift
+//  ClownRoyale
+//
+//  Created by Matthias Riener on 22.03.21.
+//
+
+import SwiftUI
+
+struct GamePerspectiveOwnView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GamePerspectiveOwnView_Previews: PreviewProvider {
+    static var previews: some View {
+        GamePerspectiveOwnView()
+    }
+}
