70b8caa4774444f536a5a1172d5bb764436f371e
Matthias Riener <matti.riener@gmail.com>
2021-03-22 09:34:12 +0100

    added file for gameUI

---
+++ src/frontend/ClownRoyale/ClownRoyale/GamePerspectiveOwn.swift
@@ -0,0 +1,20 @@
+//
+//  GamePerspectiveOwn.swift
+//  ClownRoyale
+//
+//  Created by Matthias Riener on 22.03.21.
+//
+
+import SwiftUI
+
+struct GamePerspectiveOwn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GamePerspectiveOwn_Previews: PreviewProvider {
+    static var previews: some View {
+        GamePerspectiveOwn()
+    }
+}
