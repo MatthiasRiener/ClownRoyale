9f968565b8be8383abeafa028dfd38d2de0bf780
LukasFriesenecker <friesilukas@gmail.com>
2021-03-24 18:11:15 +0100

    dots

--- src/frontend/ClownRoyale/ClownRoyale/BoutiqueView.swift
+++ src/frontend/ClownRoyale/ClownRoyale/BoutiqueView.swift
@@ -14,7 +14,7 @@ struct BoutiqueTop: View {
                 HStack {
                     VStack {
                         
-                    }.frame(width: UIScreen.screenWidth * 0.2, height: UIScreen.screenHeight * 0.1, alignment: .center)
+                    }.frame(width: UIScreen.screenWidth * 0.25, height: UIScreen.screenHeight * 0.1, alignment: .center)
                     
                     VStack {
                         Text("Clown Boutique")
@@ -22,7 +22,7 @@ struct BoutiqueTop: View {
                             .font(.system(size: 26))
                             .bold()
                     }
-                    .frame(width: UIScreen.screenWidth * 0.6, height: UIScreen.screenHeight * 0.1, alignment: .center)
+                    .frame(width: UIScreen.screenWidth * 0.5, height: UIScreen.screenHeight * 0.1, alignment: .center)
                     
                     VStack {
                         Text("1536")
@@ -30,7 +30,7 @@ struct BoutiqueTop: View {
                             .font(.system(size: 20))
                             .bold()
                     }
-                    .frame(width: UIScreen.screenWidth * 0.2, height: UIScreen.screenHeight * 0.1, alignment: .center)
+                    .frame(width: UIScreen.screenWidth * 0.25, height: UIScreen.screenHeight * 0.1, alignment: .center)
                 }
                 .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.1, alignment: .center)
             }
@@ -46,7 +46,54 @@ struct BoutiqueTop: View {
 struct BoutiqueMiddle: View {
     var body: some View {
         VStack {
+            HStack {
+                
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.05, alignment: .top)
+            .background(Color.white)
             
+            HStack {
+                Text("TODAY")
+                    .foregroundColor(Color("ClownGray"))
+                    .font(.system(size: 26))
+                    .bold()
+                    .padding()
+                Spacer()
+                Text("8h 49min")
+                    .foregroundColor(Color("ClownGray"))
+                    .font(.system(size: 16))
+                    .padding()
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.4, alignment: .top)
+            .background(Color.white)
+            
+            HStack {
+                RoundedRectangle(cornerRadius: 1000.0)
+                    .fill(Color.blue)
+                    .frame(width: 7.5, height: 7.5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, 1)
+                RoundedRectangle(cornerRadius: 1000.0)
+                    .fill(Color.gray)
+                    .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, 1)
+                
+                RoundedRectangle(cornerRadius: 1000.0)
+                    .fill(Color.gray)
+                    .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, 1)
+                RoundedRectangle(cornerRadius: 1000.0)
+                    .fill(Color.gray)
+                    .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, 1)
+                
+                RoundedRectangle(cornerRadius: 1000.0)
+                    .fill(Color.gray)
+                    .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    .padding(.trailing, 1)
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.05, alignment: .top)
+            .background(Color.white)
+
         }
         .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.5, alignment: .top)
         .background(Color.white)
@@ -56,7 +103,57 @@ struct BoutiqueMiddle: View {
 struct BoutiqueBottom: View {
     var body: some View {
         VStack {
+            HStack {
+                
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.04, alignment: .top)
+            .background(Color.white)
             
+            HStack {
+                Text("MONTHLY")
+                    .foregroundColor(Color("ClownGray"))
+                    .font(.system(size: 26))
+                    .bold()
+                    .padding()
+                Spacer()
+                Text("6T 8h")
+                    .foregroundColor(Color("ClownGray"))
+                    .font(.system(size: 16))
+                    .padding()
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.06, alignment: .top)
+            .background(Color.white)
+            
+            ZStack {
+                HStack {
+                    RoundedRectangle(cornerRadius: 1000.0)
+                        .fill(Color.blue)
+                        .frame(width: 7.5, height: 7.5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing, 1)
+                    RoundedRectangle(cornerRadius: 1000.0)
+                        .fill(Color.gray)
+                        .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing, 1)
+                    
+                    RoundedRectangle(cornerRadius: 1000.0)
+                        .fill(Color.gray)
+                        .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing, 1)
+                    RoundedRectangle(cornerRadius: 1000.0)
+                        .fill(Color.gray)
+                        .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing, 1)
+                    
+                    RoundedRectangle(cornerRadius: 1000.0)
+                        .fill(Color.gray)
+                        .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        .padding(.trailing, 1)
+                }
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.05, alignment: .center)
+                .background(Color.white)
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.25, alignment: .bottom)
+            .background(Color.green)
         }
         .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.35, alignment: .top)
         .background(Color.white)
