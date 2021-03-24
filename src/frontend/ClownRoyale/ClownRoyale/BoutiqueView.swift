a79c43aac454039c40bb3a860106c9164f544073
LukasFriesenecker <friesilukas@gmail.com>
2021-03-24 18:59:00 +0100

    :|

--- src/frontend/ClownRoyale/ClownRoyale/BoutiqueView.swift
+++ src/frontend/ClownRoyale/ClownRoyale/BoutiqueView.swift
@@ -36,10 +36,10 @@ struct BoutiqueTop: View {
             }
             .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.14, alignment: .bottom)
             .background(Color.white)
-            .shadow(color: Color.black.opacity(0.8), radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 1.0)
         }
         .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.15, alignment: .top)
         .background(Color.white)
+        .shadow(color: Color.black.opacity(0.8), radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 1.0)
     }
 }
 
@@ -64,7 +64,68 @@ struct BoutiqueMiddle: View {
                     .font(.system(size: 16))
                     .padding()
             }
-            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.4, alignment: .top)
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.06, alignment: .top)
+            .background(Color.white)
+            
+            ZStack {
+                HStack {
+                    HStack {
+                        VStack {
+                            HStack {
+                                Text("FUNNY GLASSES")
+                                    .foregroundColor(Color("ClownGray"))
+                                    .font(.system(size: 18))
+                                    .bold()
+                            }
+                            .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.04, alignment: .bottom)
+                            
+                            HStack {
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("ClownGreen"))
+                                    .frame(width: UIScreen.screenHeight * 0.06, height: UIScreen.screenHeight * 0.06, alignment: .center)
+                                    .overlay(
+                                        Image("Smiley")
+                                            .resizable()
+                                            .cornerRadius(1000)
+                                            .ignoresSafeArea()
+                                            .frame(width: UIScreen.screenHeight * 0.03, height: UIScreen.screenHeight * 0.03, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                                    )
+                                    .shadow(color: .gray, radius: 3, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                                
+                                RoundedRectangle(cornerRadius: 10)
+                                    .fill(Color("ClownYellow"))
+                                    .frame(width: UIScreen.screenWidth * 0.4, height: UIScreen.screenHeight * 0.06, alignment: .center)
+                                    .overlay(
+                                        Text("800")
+                                            .font(.system(size: 20))
+                                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                                            .foregroundColor(.white)
+                                    )
+                                    .shadow(color: .gray, radius: 3, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                            }
+                            .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.08, alignment: .top)
+                        }
+                        .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.12, alignment: .bottom)
+                    }
+                    .frame(width: UIScreen.screenWidth * 0.9, height: UIScreen.screenHeight * 0.26, alignment: .bottom)
+                    .background(Color.white)
+                    .cornerRadius(20.0)
+                    .shadow(color: .gray, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                }
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.34, alignment: .bottom)
+                
+                HStack {
+                    HStack {
+                        
+                    }
+                    .frame(width: UIScreen.screenHeight * 0.2, height: UIScreen.screenHeight * 0.2, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(1000.0)
+                    .shadow(color: .gray, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                }
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.34, alignment: .top)
+            }
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.34, alignment: .top)
             .background(Color.white)
             
             HStack {
@@ -91,7 +152,7 @@ struct BoutiqueMiddle: View {
                     .frame(width: 5, height: 5, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
                     .padding(.trailing, 1)
             }
-            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.05, alignment: .top)
+            .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.05, alignment: .center)
             .background(Color.white)
 
         }
@@ -124,7 +185,29 @@ struct BoutiqueBottom: View {
             .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.06, alignment: .top)
             .background(Color.white)
             
-            ZStack {
+            VStack {
+                HStack {
+                    HStack {
+                        
+                    }
+                    .frame(width: UIScreen.screenWidth * 0.5, height: UIScreen.screenHeight * 0.17, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(10.0)
+                    .shadow(color: .gray, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    .padding(.leading, 90)
+                    
+                    HStack {
+                        
+                    }
+                    .frame(width: UIScreen.screenWidth * 0.5, height: UIScreen.screenHeight * 0.17, alignment: .center)
+                    .background(Color.white)
+                    .cornerRadius(10.0)
+                    .shadow(color: .gray, radius: 5, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
+                    .padding(.leading, 50)
+                }
+                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.20, alignment: .center)
+                .background(Color.white)
+                
                 HStack {
                     RoundedRectangle(cornerRadius: 1000.0)
                         .fill(Color.blue)
@@ -153,7 +236,7 @@ struct BoutiqueBottom: View {
                 .background(Color.white)
             }
             .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.25, alignment: .bottom)
-            .background(Color.green)
+            .background(Color.white)
         }
         .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight * 0.35, alignment: .top)
         .background(Color.white)
