5e8a9b3cdcce29f062721a926013254ac46f9e25
Matthias Riener <matti.riener@gmail.com>
2021-04-20 17:02:37 +0200

    änderungen für 2 screen2 2

---
+++ src/ClownRoyale/ClownRoyale/OwnPerspectiveViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OwnPerspectiveViewController.swift
+//  ClownRoyale
+//
+//  Created by Matthias Riener on 20.04.21.
+//
+
+import UIKit
+
+class OwnPerspectiveViewController: ViewController {
+    override func viewDidLoad() {
+        print("VIREW dIeD Load")
+    }
+}
