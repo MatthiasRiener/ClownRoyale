3492cc18f695ccb45e5ba3f645d199164275ccd9
LukasFriesenecker <friesilukas@gmail.com>
2021-04-21 15:14:28 +0200

    shop + profile

---
+++ src/ClownRoyale/ClownRoyale/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ClownRoyale
+//
+//  Created by Lukas Friesenecker on 21.04.21.
+//
+
+import Foundation
+
+class ProfileViewController: ViewController {
+
+}
---
+++ src/ClownRoyale/ClownRoyale/ShopViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ShopViewController.swift
+//  ClownRoyale
+//
+//  Created by Lukas Friesenecker on 21.04.21.
+//
+
+import Foundation
+
+class ShopViewController: ViewController {
+
+}
