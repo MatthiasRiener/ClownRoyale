3286b610b2f916edc3e1190e67d7102d02c6bf23
MatthiasRiener <matti.riener@gmail.com>
2021-05-03 19:13:23 +0200

    added refresh token url

--- src/server/routes/authentication/auth.js
+++ src/server/routes/authentication/auth.js
@@ -2,22 +2,24 @@ var router = require('express').Router();
 const axios = require('axios');
 const qs = require('qs');
 
+const baseURL = "http://localhost:8080/auth/realms/";
+const realmName = "clown";
+const subURL = "/protocol/openid-connect/token";
+
 router.post('/login', (req, res) => {
 
-    const baseURL = "http://localhost:8080/auth/realms/";
-    const realmName = "clown";
-    const subURL = "/protocol/openid-connect/token";
+
 
     const username = req.body.username;
     const password = req.body.password;
 
     console.log(username, password);
 
-    const request_options =  {
+    const request_options = {
         headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
+            'Content-Type': 'application/x-www-form-urlencoded'
         }
-      }
+    }
 
     const data = {
         "client_id": "node-client",
@@ -27,19 +29,46 @@ router.post('/login', (req, res) => {
         "password": req.body.password,
         "grant_type": "password"
     }
-    
-    axios.post(baseURL + realmName + subURL, qs.stringify(data) , request_options)
-    .then((response) => {
-        console.log(response);
-        res.send({"isLogin": true, "access": response.data.access_token, "refresh": response.data.refresh_token})
-    })
-    .catch((error) => {
-        res.send({"error": "Es gab einen Fehler!"})
-        console.error(error);
-    })
+
+    axios.post(baseURL + realmName + subURL, qs.stringify(data), request_options)
+        .then((response) => {
+            console.log(response);
+            res.send({ "isLogin": true, "access": response.data.access_token, "refresh": response.data.refresh_token })
+        })
+        .catch((error) => {
+            res.send({ "error": "Es gab einen Fehler!" })
+            console.error(error);
+        })
 });
 
 router.post('/refreshToken', (req, res) => {
+
+    const refresh = req.body.refresh_token;
+
+    const request_options = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    }
+
+    const data = {
+        "client_id": "node-client",
+        "scope": "openid",
+        "client_secret": "6a29eee4-de06-4492-84b7-3618a57111e8",
+        "refresh_token": refresh,
+        "grant_type": "refresh_token"
+    }
+
+    axios.post(baseURL + realmName + subURL, qs.stringify(data), request_options)
+        .then((response) => {
+            console.log(response);
+            res.send({ "isRefresh": true, "access": response.data.access_token, "refresh": response.data.refresh_token })
+        })
+        .catch((error) => {
+            res.send({ "error": "Es gab einen Fehler!" })
+            console.error(error);
+        })
+
     /*
         Method: POST
         URL: https://keycloak.example.com/auth/realms/myrealm/protocol/openid-connect/token
@@ -55,14 +84,14 @@ router.post('/refreshToken', (req, res) => {
 
 router.post('/register', (req, res) => {
     console.log("Register...");
-    res.send({"res": 1});
+    res.send({ "res": 1 });
 })
 
 
 const users = ["Jan", "Lukas", "Simon", "Matthias"];
 var requestCounter = 0;
 router.get('/fakeLogin', (req, res) => {
-    res.send({"username": users[requestCounter]})
+    res.send({ "username": users[requestCounter] })
 
     requestCounter == users.length - 1 ? requestCounter = 0 : requestCounter++;
 })
