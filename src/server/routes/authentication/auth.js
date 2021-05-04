f1f7bf8708a215c2cd951a085554730353a85d06
MatthiasRiener <matti.riener@gmail.com>
2021-05-04 21:03:26 +0200

    funny

--- src/server/routes/authentication/auth.js
+++ src/server/routes/authentication/auth.js
@@ -103,7 +103,9 @@ function getAdminToken() {
     }
 
     const data = {
-        "client_id": "admin-cli",
+        "client_id": "node-client",
+        "scope": "openid",
+        "client_secret": "6a29eee4-de06-4492-84b7-3618a57111e8",
         "username": "clownadmin",
         "password": "afrocircus",
         "grant_type": "password"
