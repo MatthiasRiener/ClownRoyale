ecee7955398f58b8c335f91cc9c2c3c456b259a0
MatthiasRiener <matti.riener@gmail.com>
2021-05-19 19:18:45 +0200

    ma

--- src/server/repository/AuthenticationRepository.js
+++ src/server/repository/AuthenticationRepository.js
@@ -36,17 +36,6 @@ async function getUsersFromArray(users) {
     return await users.map((user) => getUser(user));
 }
 
-function getUsersFromArray(users) {
-    var response = [];
-    users.forEach((u) => {
-        getUser(u).then((doc) => {
-            response.push(doc);
-        });
-    });
-
-    return response;
-}
-
 
 async function getUser(u_id) {
     const doc = await UserModel.findOne({ u_id: u_id }).exec();
